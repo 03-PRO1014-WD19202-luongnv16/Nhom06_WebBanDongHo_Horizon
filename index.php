6d0c13453591bf103d2d29b0c29f66d1c0526cca
anhnpdph47088 <anhnpdph47088@fpt.edu.vn>
2024-07-16 09:31:29 +0700

    commit change

--- index.php
+++ index.php
@@ -19,7 +19,34 @@ if (isset($_GET['act'])) {
                 $showProductList = false;
             }
             break; 
-            
+        case 'new':
+                if(isset($_POST['kyw'])&&($_POST['kyw']!="")){
+                    $kyw=$_POST['kyw'];
+                 }else{
+                     $kyw="";
+                 }
+                 if(isset($_GET['id_dm'])&&($_GET['id_dm']>0)){
+                     $id_dm=$_GET['id_dm'];                               
+                     
+                 }else{
+                     $id_dm=0;
+                 }
+                 $dssp=loadall_sanpham($kyw, $id_dm);
+                 $tendm = loadone_sanpham($id_dm);
+                 include "view/sanpham.php";
+                 break;
+        case 'hot':
+                include "/view/hot.php";
+                $showProductList = false;
+                break;
+        case 'famous':
+                include "/view/famous.php";
+                $showProductList = false;
+                break;
+        case 'limited';
+                include "/view/limited.php";
+                $showProductList = false;
+                break;   
         case 'addToCart':  
             if (isset($_GET['id_sp'])) {  
                 $id_sp = $_GET['id_sp'];  
